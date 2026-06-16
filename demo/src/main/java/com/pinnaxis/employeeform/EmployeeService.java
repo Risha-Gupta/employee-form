b0e041cbe69c87d114b54d43597f59566305f774
Risha <rishagupta87503@gmail.com>
2026-06-16 13:46:44 +0530

    Create service layer

---
+++ demo/src/main/java/com/pinnaxis/employeeform/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.pinnaxis.employeeform;
+
+import com.pinnaxis.employeeform.EmployeeForm;
+import com.pinnaxis.employeeform.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EmployeeService{
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //Save Employee to H2 database
+    public void saveEmployee(EmployeeForm employee){
+        employeeRepository.save(employee);
+    }
+
+    //Get all employees from database
+    public List<EmployeeForm> getEmployees(){
+        return employeeRepository.findAllByOrderBySubmissionTimeAsc();
+    }
+}
