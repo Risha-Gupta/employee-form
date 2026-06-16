fedab0cdafbe375bc3ea010f74fd45195d92d62d
Risha <rishagupta87503@gmail.com>
2026-06-16 16:23:59 +0530

    Recreated employee respository

--- demo/src/main/java/com/pinnaxis/employeeform/repository/EmployeeRepository.java
+++ demo/src/main/java/com/pinnaxis/employeeform/repository/EmployeeRepository.java
@@ -1,14 +1,10 @@
 package com.pinnaxis.employeeform.repository;
+import com.pinnaxis.employeeform.entity.EmployeeForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.pinnaxis.employeeform.entity.EmployeeForm;
-
 import java.util.List;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeForm, Long> {
+public interface EmployeeRepository extends JpaRepository<EmployeeForm, Long>{
     List<EmployeeForm> findAllByOrderBySubmissionTimeAsc();
 }
-
-
